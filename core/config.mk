95818c370ad6b07730b3a193fe8d4aa5ca2e6e8d
Brian Harring <brian.harring@intel.com>
2011-05-08 00:03:11 -0700

    refactor private->prebuilt machinery
    New form basically extracts all relevant ABS metadata so that
    the prebuilt form can use it; intention is to make the prebuilt
    form basically a full drop in for the private form without any
    manual intervention required.
    
    Adds two templates; $(BUILD_PREBUILT_BUNDLE_CREATE) and
    $(BUILD_PREBUILT_BUNDLE).
    
    For creation, three vars influence it; PREBUILT_PROJECT
    (grouping of modules), PREBUILT_MODULES (list of modules to scan
    and convert to prebuilts), and PREBUILT_INSTALL_DIR (optional
    subdirectory where prebuilt outputs should be placed); output is
    written to for each PREBUILT_MODULES word, the module is written to
    $(OUT_DIR)/prebuilt/$(PREBUILT_PROJECT)/$(CUSTOM_BOARD)/$(THE-MODULE).
    W/in PREBUILT/$(PREBUILT_PROJECT), a usable Android.mk is generated that
    uses the bundle machinery; basically is intended to handle it all.
    
    For the bundle usage side, it's basically automatic; needs LOCAL_PATH to
    know where to scan for bundles; it honors BUNDLE_TARGETS if specified-
    this is the list of bundles to expose to ABS.  If unspecified, it scans
    the directory and autoidentifies the bundles available.
    
    Current limitations:
    1) doesn't yet honor path overrides.
    2) doesn't handle modules that have multiple INSTALLED files; there is only
    one the author is aware of that is valid.
    3) machinery on bundle side is ready for host, but creation side doesn't
    yet fully identify if it's a host target or not, thus can't yet handle host.
    4) can handle LOCAL_COPY_HEADERS but not individual headers set
    specified by BUILD_COPY_HEADERS
    
    Change-Id: I1eac973f6924d8e2540337f9ac3c5d06cf945281
    Signed-off-by: Brian Harring <brian.harring@intel.com>
    Signed-off-by: Matt Gumbel <matthew.k.gumbel@intel.com>

--- core/config.mk
+++ core/config.mk
@@ -74,6 +74,8 @@ BUILD_DROIDDOC:= $(BUILD_SYSTEM)/droiddoc.mk
 BUILD_COPY_HEADERS := $(BUILD_SYSTEM)/copy_headers.mk
 BUILD_NATIVE_TEST := $(BUILD_SYSTEM)/native_test.mk
 BUILD_HOST_NATIVE_TEST := $(BUILD_SYSTEM)/host_native_test.mk
+BUILD_PREBUILT_BUNDLE_CREATE := $(BUILD_SYSTEM)/prebuilt_bundle_create.mk
+BUILD_PREBUILT_BUNDLE := $(BUILD_SYSTEM)/prebuilt_bundle.mk
 
 -include cts/build/config.mk
 
