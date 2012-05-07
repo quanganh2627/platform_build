9f8b0b3dbb83eb22b689a9fa46aeeab7938ce3d0
Pauli Nieminen <pauli.nieminen@linux.intel.com>
2012-05-07 21:39:57 +0300

    Default to -fno-omit-frame-pointer
    Android assumes it can do frame pointer based unwinding so we have to
    disable gcc default optimization. The best possible case would be making
    unwinding to use .eh_frames but that is some work because .eh_frame is
    subset of dwarf debug information.
    
    To make debugging easier disabling frame pointer optimization makes
    sense for now. With this patch I can get working unwinding when crash
    happens.
    
    If .eh_frame unwinding is implemented this patch can be reverted to
    allow one extra register for compiler optimization.
    
    For: AIA-449
    Change-Id: I56e31af1e9c99fb08118923632e44ac195e93c8c
    Depends-Change-Id: I544d3cbc4d3cad28441f1b923da5ecd5a6762b33
    Signed-off-by: Pauli Nieminen <pauli.nieminen@linux.intel.com>

--- core/combo/TARGET_linux-x86.mk
+++ core/combo/TARGET_linux-x86.mk
@@ -101,6 +101,7 @@ TARGET_GLOBAL_CFLAGS += \
 			-fstrict-aliasing \
 			-funswitch-loops \
 			-funwind-tables \
+			-fno-omit-frame-pointer \
 			-fstack-protector
 
 android_config_h := $(call select-android-config-h,target_linux-x86)
